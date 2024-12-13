eda1fcd0490efd1883ee16eb505322ce1bc33947
Marty Parada <martin.p1131@gmail.com>
2024-12-13 09:30:58 -0500

    feat(hosting-consult): show avaliable cities in enumerated format

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,64 +3,72 @@ package org.example;
 import java.util.*;
 
 public class Main {
+    static List<Map<String, Object>> alojamientos = Arrays.asList(
+            // Hoteles
+            Map.of(
+                    "nombre", "Hotel Paraíso",
+                    "ciudad", "Cartagena",
+                    "tipoAlojamiento", "Hotel",
+                    "calificacion", 5,
+                    "precioPorNoche", 200000
+
+            ),
+            Map.of(
+                    "nombre", "Hotel Real",
+                    "ciudad", "Bogotá",
+                    "tipoAlojamiento", "Hotel",
+                    "calificacion", 4,
+                    "precioPorNoche", 180000
+            ),
+            // Apartamentos
+            Map.of(
+                    "nombre", "Apartamento Luna",
+                    "ciudad", "Medellín",
+                    "tipoAlojamiento", "Apartamento",
+                    "calificacion", 4,
+                    "precioPorNoche", 150000
+            ),
+            Map.of(
+                    "nombre", "Apartamento Sol",
+                    "ciudad", "Bogotá",
+                    "tipoAlojamiento", "Apartamento",
+                    "calificacion", 5,
+                    "precioPorNoche", 180000
+            ),
+            // Fincas
+            Map.of(
+                    "nombre", "Finca El Encanto",
+                    "ciudad", "Cartagena",
+                    "tipoAlojamiento", "Finca",
+                    "calificacion", 5,
+                    "precioPorNoche", 250000
+            ),
+            Map.of(
+                    "nombre", "Finca La Montaña",
+                    "ciudad", "Medellín",
+                    "tipoAlojamiento", "Finca",
+                    "calificacion", 4,
+                    "precioPorNoche", 220000
+            )
+
+    );
+
     public static void main(String[] args) {
-        System.out.println("Booking hotel Project");
+        System.out.println("Seleccione la ciudad donde le gustaria hospedarse");
+        ciudadesEncontradas();
+    }
 
-        List<Map<String, Object>> alojamientos = Arrays.asList(
-                // Hoteles
-                Map.of(
-                        "nombre", "Hotel Paraíso",
-                        "ciudad", "Cartagena",
-                        "tipoAlojamiento", "Hotel",
-                        "calificacion", 5,
-                        "precioPorNoche", 200000
-
-                ),
-                Map.of(
-                        "nombre", "Hotel Real",
-                        "ciudad", "Bogotá",
-                        "tipoAlojamiento", "Hotel",
-                        "calificacion", 4,
-                        "precioPorNoche", 180000
-                ),
-                // Apartamentos
-                Map.of(
-                        "nombre", "Apartamento Luna",
-                        "ciudad", "Medellín",
-                        "tipoAlojamiento", "Apartamento",
-                        "calificacion", 4,
-                        "precioPorNoche", 150000
-                ),
-                Map.of(
-                        "nombre", "Apartamento Sol",
-                        "ciudad", "Bogotá",
-                        "tipoAlojamiento", "Apartamento",
-                        "calificacion", 5,
-                        "precioPorNoche", 180000
-                ),
-                // Fincas
-                Map.of(
-                        "nombre", "Finca El Encanto",
-                        "ciudad", "Cartagena",
-                        "tipoAlojamiento", "Finca",
-                        "calificacion", 5,
-                        "precioPorNoche", 250000
-                ),
-                Map.of(
-                        "nombre", "Finca La Montaña",
-                        "ciudad", "Medellín",
-                        "tipoAlojamiento", "Finca",
-                        "calificacion", 4,
-                        "precioPorNoche", 220000
-                )
-
-        );
+    public static void ciudadesEncontradas() {
         Set<String> ciudades = new HashSet<>();
         for (Map<String, Object> alojamiento : alojamientos) {
             ciudades.add((String) alojamiento.get("ciudad"));
         }
-            
-
+        int contador = 1;
+        for (String ciudad : ciudades) {
+            System.out.println(contador + ". " + ciudad);
+            contador++;
+        }
 
     }
+
 }
