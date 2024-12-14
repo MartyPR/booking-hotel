61830fe28375f9167f39d822748066a0468be7cc
Marty Parada <martin.p1131@gmail.com>
2024-12-13 21:05:24 -0500

    fix(hosting-consult): add new data without objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,32 @@ import java.util.*;
 
 
 public class Main {
+    static  String[] nombres = {
+            "Hotel Paraíso", "Hotel Real", "Apartamento Luna",
+            "Apartamento Sol", "Finca El Encanto", "Finca La Montaña"
+    };
+    static String[] ciudades2 = {
+            "Cartagena", "Bogotá", "Medellín", "Bogotá",
+            "Cartagena", "Medellín"
+    };
+    static String[] tipos = {
+            "Hotel", "Hotel", "Apartamento", "Apartamento",
+            "Finca", "Finca"
+    };
+    int[] calificaciones = {5, 4, 4, 5, 5, 4};
+    int[] preciosPorNoche = {200000, 180000, 150000, 180000, 250000, 220000};
+
+
     static ArrayList<String> ciudades = new ArrayList<>();
     static ArrayList<String> tipoAlojamientos = new ArrayList<>();
+
     //alojamientos
     static List<Map<String, Object>> alojamientos = Arrays.asList(
             // Hoteles
             Map.of(
                     "nombre", "Hotel Paraíso",
                     "ciudad", "Cartagena",
-                    "tipoAlojamiento", "Hotel",
-                    "calificacion", 5,
-                    "precioPorNoche", 200000
-
+                    "tipoAlojamiento", "Hotel"
             ),
             Map.of(
                     "nombre", "Hotel Real",
@@ -60,49 +74,51 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         ciudadesEncontradas();
-        tiposAlojamientoEncontradas();
-        while (true) {
-
-            int opcionCiudad = seleccionarOpcion(scanner, "Seleccione la ciudad donde le gustaria hospedarse", ciudades);
-            if (opcionCiudad == -1) continue;
-
-            int opcionTipoAlojamiento = seleccionarOpcion(scanner, "Seleccione la tipo de alojamiento donde le gustaria hospedarse", tipoAlojamientos);
-            if (opcionTipoAlojamiento == -1) continue;
-
-//            int fechaInicio  = obtenerEntradaValida(scanner, "Ingrese  el día de inicio del hospedaje: ");
-//            int fechaFin  = obtenerEntradaValida(scanner, "Ingrese el día de finalización del hospedaje: ");
-//            int cantidadAdultos = obtenerEntradaValida(scanner, "Ingrese la cantidad de adultos: ");
-//            int cantidadNinos = obtenerEntradaValida(scanner, "Ingrese la cantidad de niños: ");
-//            int cantidadHabitaciones = obtenerEntradaValida(scanner, "Ingrese la cantidad de habitaciones: ");
-            int fechaInicio = 10;
-            int fechaFin = 15;
-            int cantidadAdultos = 3;
-            int cantidadNinos = 2;
-            int cantidadHabitaciones = 2;
-            buscarAlojamientos(ciudades.get(opcionCiudad), tipoAlojamientos.get(opcionTipoAlojamiento), fechaInicio, fechaFin, cantidadAdultos, cantidadNinos, cantidadHabitaciones);
-
-
-            break;
-
-        }
+//        tiposAlojamientoEncontradas();
+//        while (true) {
+//
+//            int opcionCiudad = seleccionarOpcion(scanner, "Seleccione la ciudad donde le gustaria hospedarse", ciudades);
+//            if (opcionCiudad == -1) continue;
+//
+//            int opcionTipoAlojamiento = seleccionarOpcion(scanner, "Seleccione la tipo de alojamiento donde le gustaria hospedarse", tipoAlojamientos);
+//            if (opcionTipoAlojamiento == -1) continue;
+//
+////            int fechaInicio  = obtenerEntradaValida(scanner, "Ingrese  el día de inicio del hospedaje: ");
+////            int fechaFin  = obtenerEntradaValida(scanner, "Ingrese el día de finalización del hospedaje: ");
+////            int cantidadAdultos = obtenerEntradaValida(scanner, "Ingrese la cantidad de adultos: ");
+////            int cantidadNinos = obtenerEntradaValida(scanner, "Ingrese la cantidad de niños: ");
+////            int cantidadHabitaciones = obtenerEntradaValida(scanner, "Ingrese la cantidad de habitaciones: ");
+//            int fechaInicio = 10;
+//            int fechaFin = 15;
+//            int cantidadAdultos = 3;
+//            int cantidadNinos = 2;
+//            int cantidadHabitaciones = 2;
+//            buscarAlojamientos(ciudades.get(opcionCiudad), tipoAlojamientos.get(opcionTipoAlojamiento), fechaInicio, fechaFin, cantidadAdultos, cantidadNinos, cantidadHabitaciones);
+//
+//
+//            break;
+//
+//        }
     }
 
     public static void buscarAlojamientos(String ciudad, String tipoAlojamiento, int fechaInicio, int fechaFin,
                                           int cantidadAdultos, int cantidadNinos, int cantidadHabitaciones) {
         System.out.println(ciudad);
         List<Map<String, Object>> alojamientoCiudad = filtrarPorSeleccion("ciudad", ciudad, alojamientos);
-        List<Map<String, Object>> alojamientoPorTipo= filtrarPorSeleccion("tipoAlojamiento", tipoAlojamiento, alojamientoCiudad);
-        System.out.println(alojamientoPorTipo);
+        List<Map<String, Object>> alojamientoPorTipo = filtrarPorSeleccion("tipoAlojamiento", tipoAlojamiento, alojamientoCiudad);
+
 
     }
 
     public static void ciudadesEncontradas() {
         ciudades.clear();
-        for (Map<String, Object> alojamiento : alojamientos) {
-            if (!ciudades.contains((String) alojamiento.get("ciudad"))) {
-                ciudades.add((String) alojamiento.get("ciudad"));
+        for (String ciudad : ciudades2) {
+            if (!ciudades.contains((String) ciudad)) {
+                ciudades.add((String) ciudad);
+                System.out.println(ciudad);
             }
         }
     }
@@ -134,7 +150,7 @@ public class Main {
             try {
                 opcion = scanner.nextInt();
                 if (opcion >= 1 && opcion <= maxOption) {
-                    return opcion;
+                    return opcion - 1;
                 }
                 if (opcion == (maxOption + 1)) {
                     System.out.println("close");
